f5b1bcd366144bd8cd62417e9bb8c0066fa0f5a1
sigh-gone <a1@Als-MacBook-Pro.local>
2023-03-31 13:57:43 -0500

    commiting changes to archiver

--- mod/archiver.ts
+++ mod/archiver.ts
@@ -1,21 +1,11 @@
-import puppeteer from "https://deno.land/x/puppeteer@9.0.2/mod.ts";
-import {HTMLDocument} from "./records.ts";
+//@ts-ignore
+//@ts-ignore
+import { HTMLDocument } from "./records.ts";
 
 export async function archive_webpage(link:string):Promise<HTMLDocument> {
     return new Promise(async (resolve) =>{
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(link);
-        await page.waitFor(100)
-
-        //Uint8 array
-        let screenshot = await page.screenshot();
-        //string
-        // @ts-ignore
-        const text = await page.evaluate(() => document.querySelector('*').outerHTML);
-
-        let record = new HTMLDocument(link,screenshot,text);
-        await browser.close();
+        const page = await (await fetch(link)).body;
+        let record = new HTMLDocument(link,page);
         resolve(record)
     })
 }
--- mod/records/html_record.ts
+++ mod/records/html_record.ts
@@ -1,24 +1,20 @@
+//@ts-ignore
 import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 
 export class HTMLDocument {
     public url;
-    public screenshot;
     public body;
+    private hmtl
 
 
-    constructor(_url:string,_screenshot:any, _body:any) {
+    constructor(_url:string, _body:any) {
         this.url = _url;
-        this.screenshot = _screenshot;
         this.body = _body;
-    }
-
-    public get_document():any{
-       return new DOMParser().parseFromString(this.body, 'text/html');
+        this.hmtl = new DOMParser().parseFromString(this.body, 'text/html');
     }
 
     public links():Array<string>{
-        const document: any = this.get_document();
-        let link_array:Array<any> = document.querySelectorAll('a');
+        let link_array:Array<any> = this.hmtl.querySelectorAll('a');
         //@ts-ignore
         let links:Array<string> = [];
         for(let i =0; i <link_array.length;i++){
@@ -35,15 +31,14 @@ export class HTMLDocument {
     }
 
     public domain_links():Array<string>{
-        const document: any = this.get_document();
-        let link_array:Array<any> = document.querySelectorAll('a');
+        let link_array:Array<any> = this.hmtl.querySelectorAll('a');
         //@ts-ignore
         let links:Array<string> = [];
         for(let i =0; i <link_array.length;i++){
             if(link_array[i].attributes.href.length >0){
                 try{
                     let url = new URL(link_array[i].attributes.href);
-                    if(this.is_domain(url.href)){
+                    if(this.host_match(url.href)){
                         links.push(url.href)
                     }
                 }catch{
@@ -53,16 +48,15 @@ export class HTMLDocument {
         }
         return links;
     }
-    public non_domain_links():Array<string>{
-        const document: any = this.get_document();
-        let link_array:Array<any> = document.querySelectorAll('a');
+    public non_host_links():Array<string>{
+        let link_array:Array<any> = this.hmtl.querySelectorAll('a');
         //@ts-ignore
         let links:Array<string> = [];
         for(let i =0; i <link_array.length;i++){
             if(link_array[i].attributes.href.length >0){
                 try{
                     let url = new URL(link_array[i].attributes.href);
-                    if(!this.is_domain(url.href)){
+                    if(!this.host_match(url.href)){
                         links.push(url.href)
                     }
                 }catch{
@@ -75,8 +69,7 @@ export class HTMLDocument {
 
     public curated_links(regex_string:any):Array<string>{
         let regex = new RegExp(regex_string)
-        const document: any = this.get_document();
-        let link_array:Array<any> = document.querySelectorAll('a');
+        let link_array:Array<any> = this.hmtl.querySelectorAll('a');
         //@ts-ignore
         let links:Array<string> = [];
         for(let i =0; i <link_array.length;i++){
@@ -99,21 +92,17 @@ export class HTMLDocument {
 
     private parse_link(link:string):string{
            let base = new URL(this.url);
-            if(link.substr(0,1).match("/")){
+            if(link.substring(0,1).match("/")){
                 return base.origin+link
             }else{
                 return base.origin+"/"+link
             }
     }
-    /*
-    australian sites will have issue
-     */
-    private is_domain(link:string):boolean{
-        let origin = new URL(this.url).origin.split(".");
-        let to_check = new URL(link).origin.split(".");
-        let origin_parsed = origin[origin.length-2]+origin[origin.length-1];
-        let to_check_parsed = to_check[to_check.length-2]+to_check[to_check.length-1];
 
-        return origin_parsed===to_check_parsed;
+    private host_match(link:string):boolean{
+        let origin = new URL(this.url).host;
+        let to_check = new URL(link).host;
+
+        return origin===to_check;
     }
 }
